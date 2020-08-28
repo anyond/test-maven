67c32b3282c53332ee7250e2932589ef124de414
muye <anyondmy@163.com>
2020-08-28 22:15:00 +0800

    创建项目，首次提交

---
+++ muye-maven-plugin/src/main/java/com/muye/plugin/MuyePlugin.java
@@ -0,0 +1,19 @@
+package com.muye.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name="muye")
+public class MuyePlugin extends AbstractMojo {
+    @Parameter
+    String sex;
+    @Parameter
+    String describe;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info(String.format("luban sex = %s,describe = %s", sex, describe));
+    }
+}
---
+++ src/test/java/TestHello.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestHello {
+    @Test
+    public void testHello() {
+        System.out.println("test run...");
+        Assert.assertEquals("1", "1");
+    }
+}
